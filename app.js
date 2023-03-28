ded80bb13d6611c986b7d58ff9ff62f70ccf540a
adhiramaa <vinitfagna@gmail.com>
2023-03-28 13:46:55 +0530

    Import the file system lib to handle the html file

--- app.js
+++ app.js
@@ -1,14 +1,25 @@
 const http = require("http");
+const fs = require("fs");
 const port = 3000;
 
 const server = http.createServer((req, res) => {
-  res.write("Yokoso you finaly get here :-)");
+  //telling the server that it has to parse a html file with a status code of 200
+  res.writeHead(200, { "content-type": "text/html" });
+  fs.readFile("index.html", (err, data) => {
+    if (err) {
+      res.writeHead(404);
+      res.write("Error file not found");
+    } else {
+      res.write(data);
+    }
+    res.end();
+  });
 });
 
 server.listen(port, (err) => {
   if (err) {
     console.log("505", console.error(err));
   } else {
-    console.log("Server listening to port" + port);
+    console.log(`Server listening to port at ${port}`);
   }
 });
